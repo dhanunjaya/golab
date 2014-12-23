48269b7347a48b2ff8029767aa8f8c54ff4988d3
dhanunjaya naidu ravada <dhanunjaya.matrix@gmail.com>
2014-12-23 19:08:36 +0530

    added switch control structure

---
+++ control_structures4/switch.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+		for i := 1; i <= 5; i++ {
+				switch i {
+						case 1: fmt.Println("one")
+						case 2: fmt.Println("two")
+						case 3: fmt.Println("three")
+						case 4: fmt.Println("four")
+						case 5: fmt.Println("five")
+						default: fmt.Println("unknown number")
+				}	
+		}		
+}
