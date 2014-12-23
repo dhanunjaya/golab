75db6dbc7f3851fead7e1b396f1557b0d1e0ea69
dhanunjaya naidu ravada <dhanunjaya.matrix@gmail.com>
2014-12-23 16:11:12 +0530

    this program talks about types in Go

---
+++ types2/types.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int8 = -2
+	var y uint8 = 25
+	var z int32 = -525
+	var p uint32 = 12000
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(p)
+	fmt.Println(len("Dhanunjaya"))
+}
