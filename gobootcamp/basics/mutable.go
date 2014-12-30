446b3c6d396c1643575dbc80ec36c744dfeb279b
dhanunjaya naidu ravada <dhanunjaya.matrix@gmail.com>
2014-12-30 23:59:33 +0530

    This programs talks about mutability in Go Lang

---
+++ gobootcamp/basics/mutable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Artist struct {
+		Name, Genre string
+		Songs int
+}
+
+func newRelease(a Artist) int {
+		a.Songs++
+		return a.Songs
+}
+
+func main () {
+		me := Artist{Name:"Mat", Genre:"Electro", Songs:42}
+		fmt.Printf("%s released their %d song\n", me.Name, newRelease(me))
+		fmt.Printf("%s has a total of %d songs\n", me.Name, me.Songs)
+}
