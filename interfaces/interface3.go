5eb9d7f8e6e73768955c2ac469bab7ea06208823
dhanunjaya <dhanunjaya.matrix@gmail.com>
2015-01-02 11:37:58 +0530

    This program demonstrate the usage of interfaces in go lang

--- interfaces/interface3.go
+++ interfaces/interface3.go
@@ -6,4 +6,23 @@ type Stringer interface {
 		String() string
 }
 
-type 
+type fakestring struct {
+		content string
+}
+
+func (s *fakestring) String() string {
+		return s.content
+}
+
+func printString(value interface{}) {
+		switch str := value.(type) {
+		case string: fmt.Println(str)
+		case Stringer: fmt.Println(str.String())
+		}
+}
+
+func main() {
+		str := &fakestring{content:"sachin ramesh tendulkar"}	
+		printString(str)
+		printString("virat kohli")
+} 
