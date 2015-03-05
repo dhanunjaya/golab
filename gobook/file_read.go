c66766b0f6209346956d67070a7e4cee9b847d80
dhanunjaya <dhanunjaya.matrix@gmail.com>
2015-03-05 22:55:43 +0530

    reading file in go lang

---
+++ gobook/file_read.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+	f, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Printf("Open() %s\n", err)
+		os.Exit(1)
+	}
+	buf := make([]byte, 100)
+	n, err := f.Read(buf)
+	stringVersion := string(buf)
+	fmt.Printf("%d %s", n, stringVersion)
+}
