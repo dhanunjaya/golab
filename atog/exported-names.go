a7a91517075a76f3688aca5a759d624de45b290a
dhanunjaya <dhanunjaya.matrix@gmail.com>
2015-05-18 10:42:04 +0530

    A tour of go examples - exported-names.go

---
+++ atog/exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//fmt.Println("In go anything is exported which starts with capital letter", math.pi)
+	fmt.Println("Now this works", math.Pi)
+}
