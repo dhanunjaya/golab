ce04d402f5a7860b47eba4015c71e85456eb4a0c
dhanunjaya <dhanunjaya.matrix@gmail.com>
2015-06-22 16:44:49 +0530

    Simple echo server which serves only one client

---
+++ network/echo_server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	//	"bufio"
+	//	"fmt"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		handleClient(conn)
+	}
+}
+
+func handleClient(client net.Conn) {
+	for {
+		buf := make([]byte, 4096)
+		nbytes, err := client.Read(buf)
+		if nbytes == 0 || err != nil {
+			return
+		}
+		client.Write(buf)
+	}
+}
